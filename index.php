1a7f02e08c0ae1208351bc6fa02621bb0ac61202
Your Name <bogdankasjanchyk@gmail.com>
2023-04-25 17:45:15 +0300

    my changes

--- index.php
+++ index.php
@@ -6,11 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="css\style.css">
     <title>LINGER</title>
 </head>
 <body>
         <header>
+            <div class="header">
             <div class="header__wrapper">
                 <div class="header__top_section">
                     <div class="top__section_icon_container">
@@ -45,8 +46,8 @@
                         <div class="ul_container">
                             <ul>
                                 <li><a href="#">контакти</a></li>
-                                <li><a href="#">магазин</a></li>
-                                <li class="li_new"><a href="#">військовий стан</a><sup class="new">нове</sup></li>
+                                <li><a href="#">центр підготовки</a></li>
+                                <li class="li_new"><a href="#">війна</a><sup class="new">Актуальне</sup></li>
                             </ul>
                         </div>
                         <div class="top__section_icon_container search">
@@ -58,9 +59,23 @@
                     </div>
                 </div>
 
-                .
+                <div class="header__slider_wrapper">
+                    <div class="header__slider_container">
+                        <div class="slider__divider">
+                            <div class="divider__leftside">
+                                <h2>linger запрошує вас відвідати безплатний курс військової підготовки </h2>
+                                <button id="sign" class="button">Записатись</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
 
             </div>
+            </div>
         </header>
+        <main>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum incidunt, dolorem voluptates, quo tempore, voluptatibus dignissimos expedita cumque autem iste soluta! Voluptas excepturi inventore consequuntur eveniet mollitia aut vero error voluptatem, magni animi a distinctio ad placeat assumenda illum facilis quibusdam in, ducimus quod doloribus alias hic odio totam iste? Fuga, et! Voluptatem tempore eum sapiente excepturi culpa dolore voluptatum velit odit. Voluptas, dolorem aspernatur iste, ab accusamus sunt eaque odit fugiat qui fuga natus, quis similique! Ex provident animi voluptates? Repudiandae a, pariatur accusamus esse voluptates quidem eligendi eos recusandae, ipsam vitae maxime quis necessitatibus voluptas soluta atque quasi ab rem itaque error. Laboriosam dicta autem aliquid totam qui dolor sequi dolorem? Assumenda obcaecati quod dolores explicabo soluta rem minima illum porro dolorum quas, blanditiis dolorem aliquam eaque at quibusdam id, nostrum quam maiores! Perferendis incidunt harum cumque possimus? Voluptas vero delectus tempora sit facere incidunt, ipsa facilis consequuntur non, eius numquam obcaecati! In amet minima corrupti, ex, atque impedit dolorum voluptas blanditiis commodi nobis veniam omnis ratione eligendi repellendus neque tempore! Odio, ad! Corporis numquam alias esse, accusantium consequuntur voluptas debitis recusandae quia maiores dignissimos. Nemo numquam exercitationem possimus corporis sit, tempora mollitia officia, nihil tempore minima deserunt!
+        </main>
 </body>
 </html>
