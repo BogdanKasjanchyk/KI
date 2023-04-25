014b15482739a6aaa0dea2c4dbee15287c7daa06
Your Name <bogdankasjanchyk@gmail.com>
2023-04-25 23:51:15 +0300

    my changes

--- index.php
+++ index.php
@@ -45,9 +45,9 @@
                     <div class="ul__search_container">
                         <div class="ul_container">
                             <ul>
-                                <li><a href="#">контакти</a></li>
-                                <li><a href="#">центр підготовки</a></li>
-                                <li class="li_new"><a href="#">війна</a><sup class="new">Актуальне</sup></li>
+                                <li><a href="#">Про нас</a></li>
+                                <li><a href="#">Діяльність</a></li>
+                                <li class="li_new"><a href="#">війна в україні</a><sup class="new">Актуальне</sup></li>
                             </ul>
                         </div>
                         <div class="top__section_icon_container search">
@@ -62,8 +62,8 @@
                 <div class="header__slider_wrapper">
                     <div class="header__slider_container">
                         <div class="slider__divider">
-                            <div class="divider__leftside">
-                                <h2>linger запрошує вас відвідати безплатний курс військової підготовки </h2>
+                            <div class="divider__container">
+                                <h2 class="advertisement">linger запрошує вас відвідати безплатний курс військової підготовки </h2>
                                 <button id="sign" class="button">Записатись</button>
                             </div>
                         </div>
@@ -75,7 +75,42 @@
             </div>
         </header>
         <main>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum incidunt, dolorem voluptates, quo tempore, voluptatibus dignissimos expedita cumque autem iste soluta! Voluptas excepturi inventore consequuntur eveniet mollitia aut vero error voluptatem, magni animi a distinctio ad placeat assumenda illum facilis quibusdam in, ducimus quod doloribus alias hic odio totam iste? Fuga, et! Voluptatem tempore eum sapiente excepturi culpa dolore voluptatum velit odit. Voluptas, dolorem aspernatur iste, ab accusamus sunt eaque odit fugiat qui fuga natus, quis similique! Ex provident animi voluptates? Repudiandae a, pariatur accusamus esse voluptates quidem eligendi eos recusandae, ipsam vitae maxime quis necessitatibus voluptas soluta atque quasi ab rem itaque error. Laboriosam dicta autem aliquid totam qui dolor sequi dolorem? Assumenda obcaecati quod dolores explicabo soluta rem minima illum porro dolorum quas, blanditiis dolorem aliquam eaque at quibusdam id, nostrum quam maiores! Perferendis incidunt harum cumque possimus? Voluptas vero delectus tempora sit facere incidunt, ipsa facilis consequuntur non, eius numquam obcaecati! In amet minima corrupti, ex, atque impedit dolorum voluptas blanditiis commodi nobis veniam omnis ratione eligendi repellendus neque tempore! Odio, ad! Corporis numquam alias esse, accusantium consequuntur voluptas debitis recusandae quia maiores dignissimos. Nemo numquam exercitationem possimus corporis sit, tempora mollitia officia, nihil tempore minima deserunt!
+            <div class="main__wrapper">
+                <section class="about_us">
+                    <h2 class="title">Про нас</h2>
+                    <div class="about_us__text">
+                        <div class="aspect">
+                            <p class="number">1</p>
+                            <p class="text"><span class="company">linger</span> - неприбуткова організація, яка займається дослідженням та аналізом військових конфліктів у світі. Ми були засновані у 2007 році Кімберлі Каган з метою надання точної та своєчасної інформації про важливі геополітичні події та їх наслідки для національної безпеки США та їхніх союзників. Наша штаб-квартира розташована у Вашингтоні, округ Колумбія.</p>
+                        </div>
+                        <div class="aspect">
+                            <p class="number">2</p>
+                            <p class="text">Наша місія - покращувати розуміння війни та її динаміки через ретельний дослідницький процес, який об'єднує традиційну академічну методологію з інноваційними технологіями. Ми прагнемо створювати високоякісні продукти, які можуть слугувати джерелом інформації та натхнення для політиків, експертів, журналістів, студентів та громадськості.</p>
+                        </div>
+                        <div class="aspect">
+                            <p class="number">3</p>
+                            <p class="text">Наша візія - бути провідним центром експертизи з питань вивчення війни та сприяти миротворчим розв'язанням конфліктів. Ми вважаємо, що краще розуміння причин, ходу та наслідків воєн може допомогти запобігти непотребному насильству, зменшити людське страждання та покращити глобальну стабільність.</p>
+                        </div>
+                        <div class="countries__info">
+                            <div class="aspect country">
+                                <img src="img\ua.svg" alt="" class="countries">
+                                <img src="img\iraq.svg" alt="" class="countries">
+                                <img src="img\syria.svg" alt="" class="countries">
+                                <img src="img\china.svg" alt="" class="countries">
+                                <img src="img\belarus.svg" alt="" class="countries">
+                            </div>
+                            <p class="text">*Країни, війни в яких вивчила організація Linger</p>
+                        </div>
+                    </div>
+
+                     <h2 class="title">Діяльність</h2>
+                    <div class="activity">
+                        <div class="aspect">
+                            <p class="text">Методологія досліджень LINGER спирається як на первинні, так і на вторинні джерела, що дозволяє дослідникам отримати всебічне розуміння ситуації на місцях. Для того, щоб аналізувати військові та політичні події в тій чи іншій місцевості, аналітики ISW повинні досконало розуміти системи ворожих та дружніх збройних сил. Вони також повинні розуміти демографічні показники населення, фізичний рельєф місцевості, політику та історію цієї території. Це закладає аналітичну основу для розуміння причин конкретних подій і виконання поставлених дослідницьких завдань.</p>
+                        </div>                      
+                    </div>
+                </section>               
+            </div>
         </main>
 </body>
 </html>
