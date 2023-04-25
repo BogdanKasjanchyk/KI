5b5158408ed181aa5dce0d5ac71c5ea894bde9ef
Your Name <bogdankasjanchyk@gmail.com>
2023-04-25 15:37:42 +0300

    first commit

--- index.php
+++ index.php
@@ -1,27 +1,91 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="stylesheet" href="assets/stylesheet.css">
-    <title><?= 'Start' ?></title>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="./css/style.css">
+    <title>LINGER</title>
 </head>
 <body>
-    <!-- ******HEADER****** -->
-    <header id="header" class="header">
-        <div class="container">
-            <div class="top-bar">
-                <div class="block">
+        <header>
+            <div class="header__wrapper">
+                <div class="header__top_section">
+                    <div class="top__section_icon_container">
+                        <div class="icon"><a href="#"><img src="./img/fb.svg" alt=""></a></div>
+                        <div class="icon"><a href="#"><img src="./img/inst.svg" alt=""></a></div>
+                        <div class="icon"><a href="#"><img src="./img/yt.svg" alt=""></a></div>
+                    </div>
+                    <div class="top__section_logo">
+                        <div class="logo">
+                            <a href="#"><img src="./img/linger.svg" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="top__section_language_profile">
+                        <div class="lang">
+                            <div class="lang__container">
+                                <p class="lang-switcher">
+                                    <a href="#">eng</a>
+                                </p>
+                                <p class="lang-switcher">
+                                    <a href="#">ua</a>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="profile">
+                            <a href="#"><img src="./img/profile.svg" alt=""></a>
+                        </div>
+                    </div>
+                </div>
 
+                <div class="header__ul_searh">
+                    <div class="ul__search_container">
+                        <div class="ul_container">
+                            <ul>
+                                <li><a href="#">контакти</a></li>
+                                <li><a href="#">магазини</a></li>
+                                <li><a href="#">тир</a></li>
+                            </ul>
+                        </div>
+                        <div class="top__section_icon_container search">
+                        <div class="icon"><div class="input__flex">
+                            <input type="search" name="" id="" placeholder="пошук">
+                            <img src="img\pepicons-pop_loop.svg" alt="" class="loop">
+                        </div></div>
+                            <div class="icon"><a href="#"><img src="./img/heart.svg" alt=""></a></div>
+                            <div class="icon"><a href="#"><img src="./img/scales.svg" alt=""></a></div>
+                        </div> 
+                    </div>
+                </div>
+
+                <div class="header__ul_searh">
+                    <div class="ul__search_container">
+                        <div class="ul_container">
+                            <ul class="ul_font-small">
+                                <li><a href="#">Зброя</a></li>
+                                <li><a href="#">Набої</a></li>
+                                <li><a href="#">Ножі</a></li>
+                                <li><a href="#">Тюнінг</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="header__slider_frame">
+                    <div class="slider">
+                        <div class="control">
+                            <div class="left"></div>
+                            <div class="right"></div>
+                        </div>
+                        <div class="image">
+                            <h1>linger - НАЙБІЛЬШИЙ дистриб’ютор зброї в україні</h1>
+                            <img src="" alt="">
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </header>
+        </header>
 </body>
-</html>
 </html>
