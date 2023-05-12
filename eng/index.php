4795ad9a54bb5c0700db288dcb7921f2b07f191b
Your Name <bogdankasjanchyk@gmail.com>
2023-05-12 14:05:45 +0300

    Commit

--- eng/index.php
+++ eng/index.php
@@ -17,9 +17,9 @@
                     <div class="top__section_language_profile">
                         <div class="lang">
                             <div class="lang__container">
-                                <p class="lang-switcher eng">
-                                    <a href="../eng/index.php">eng</a>
-                                </p>
+                                    <p class="lang-switcher eng">
+                                        <a href="../eng/index.php">eng</a>
+                                    </p>
                                 <p class="lang-switcher ua" tabindex="0">
                                 <a href="../ua/index.php">ua</a>
                                 </p>
